e35a975f9147f25b4ad78e3d8572719d9ba41e30
Aditya Shedge <shedge.aditya@gmail.com>
2019-02-24 15:03:04 +0530

    Added permissions for lists for admin and member users

--- app/models/permissions/admin_permission.rb
+++ app/models/permissions/admin_permission.rb
@@ -2,6 +2,13 @@ module Permissions
   class AdminPermission < BasePermission
     def set_permissions
       allow "v1/users", [:index]
+      allow "v1/lists", [:index, :create]
+      allow "v1/lists", [:show] do |list|
+        list.owner_id == user.user_id || user.lists.exists?(list.list_id)
+      end
+      allow "v1/lists", [:update, :destroy, :assign_member, :unassign_member] do |list|
+        list.owner_id == user.user_id
+      end
     end
   end
 end
--- app/models/permissions/member_permission.rb
+++ app/models/permissions/member_permission.rb
@@ -1,4 +1,10 @@
 module Permissions
   class MemberPermission < BasePermission
+    def set_permissions
+      allow "v1/lists", [:index]
+      allow "v1/lists", [:show] do |list|
+        user.lists.exists?(list.list_id)
+      end
+    end
   end
 end
